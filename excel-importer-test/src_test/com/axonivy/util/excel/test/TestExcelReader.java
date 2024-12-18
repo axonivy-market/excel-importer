485b6859160e4791ad34e3b13466b9b3011100fb
Thuy Nguyen <145430420+nntthuy-axonivy@users.noreply.github.com>
2024-12-18 08:20:51 +0700

    MARP-1386 Change date data type (#72)

--- excel-importer-test/src_test/com/axonivy/util/excel/test/TestExcelReader.java
+++ excel-importer-test/src_test/com/axonivy/util/excel/test/TestExcelReader.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.jupiter.api.Test;
@@ -25,7 +25,7 @@ class TestExcelReader {
         .contains("Firstname", "Lastname");
     assertThat(columns).contains(
         new Column("Firstname", String.class, 255), new Column("ZIP", Integer.class),
-        new Column("Amount", Double.class), new Column("Birthdate", Timestamp.class), // should be a date
+        new Column("Amount", Double.class), new Column("Birthdate", Date.class), // should be a date
         new Column("Note", String.class, 811),
         new Column("Column contains texts in incorrect number format", String.class, 255),
         new Column("Column contains both text and numeric", String.class, 255)
@@ -42,6 +42,17 @@ class TestExcelReader {
     assertThat(columns).contains(new Column("FirstName", String.class, 255),
     		new Column("LastName", String.class, 255),
     		new Column("Summary", String.class, 823));
+  }
+
+  @Test
+  void parseColumnsSeveralDateFormats_xlsx(@TempDir Path dir) throws IOException {
+    Path path = dir.resolve("customers.xlsx");
+    TstRes.loadTo(path, "sample_date_format.xlsx");
+    Workbook wb = ExcelLoader.load(path);
+    List<Column> columns = ExcelReader.parseColumns(wb.getSheetAt(0));
+    assertThat(columns).extracting(Column::getName).contains("Start date", "End date", "Date without year");
+    assertThat(columns).contains(new Column("Start date", Date.class),
+        new Column("End date", Date.class), new Column("Date without year", Date.class));
   }
 
 }
